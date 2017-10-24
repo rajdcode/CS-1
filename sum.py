09c0018c35124cc06d727ee2cdd0b34c7509efb2
rajdcode <32602401+rajdcode@users.noreply.github.com>
2017-10-24 23:12:17 +1100

    sum.py
    exercise

---
+++ sum.py
@@ -0,0 +1,8 @@
+a= input('please enter number')# input function allows to print 
+ina= int(a) # input function gives in string convert into int
+print(ina)# prints integer value
+b= input('please enter number')
+inb = int(b)
+print(inb)
+sum=ina+inb # expression adss the values and assign to sum
+print(sum)# prints value
